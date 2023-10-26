92bf245a813c2bfdd9e4deb31b95f7bcc6ab82c2
Shasanka Borah <shasanka.borah@kratosdefense.com>
2023-10-26 18:55:18 +0200

    added visitors page

--- app/(private_route)/layout.tsx
+++ app/(private_route)/layout.tsx
@@ -1,4 +1,5 @@
 import { UserPlus, Users } from "lucide-react";
+import Link from "next/link";
 import React, { ReactNode } from "react";
 const PrivateLayout = ({ children }: { children: ReactNode }) => {
   return (
@@ -10,22 +11,22 @@ const PrivateLayout = ({ children }: { children: ReactNode }) => {
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
           <ul className="space-y-2 font-medium">
             <li>
-              <a
-                href="#"
+              <Link
+                href="/home"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <UserPlus />
                 <span className="ml-3">Add visitor</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                href="/visitors"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <Users />
                 <span className="ml-3">View visitors</span>
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
--- app/(private_route)/visitors/page.tsx
+++ app/(private_route)/visitors/page.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import { QRCodeSVG } from "qrcode.react";
 const VisitorsPage = () => {
   return (
-    <div className='text-black py-10'>VisitorsPage</div>
-  )
-}
+    <>
+      <div className="text-black py-10">VisitorsPage</div>
+      <QRCodeSVG value="https://reactjs.org/" />,
+    </>
+  );
+};
 export default VisitorsPage;
